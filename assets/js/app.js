ca901fa8047b5f4b562a778c35e0399b4446ac8c
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-07-08 13:41:41 -0400

    se logran agregar las imagenes

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 		type: 'GET',
 		datatype: 'json',
 		data: {
-			'limit': '811'
+			'limit': '611' //segun la info del json
 		}
 	})
 	.done(function(response){
@@ -21,10 +21,57 @@ $(document).ready(function(){
 
 		//dentro de result están la url y el nombre del pokemon
 		data.results.forEach(function(element){
-			var name = element.name;
-			var url = element.url;
+			var namePokemon = element.name;
+			var urlPokemon = element.url;
 
-			console.log(name);
+			//console.log(namePokemon);
+
+			//realizamos otro llamado, ahora para acceder a las imagenes
+			$.ajax({
+				url: urlPokemon,
+				type: 'GET',
+				datatype: 'json'
+			})
+			.done(function(response){
+				//console.log(response);
+				imgPokemon(response);
+			})
+			.fail(function(){
+				console.log("error");
+			});
+
+			//funcion para las imagenes
+			var imgPokemon = function(miData){
+				var idPokemon = miData.id;
+				//console.log("id: " + idPokemon);
+
+				//las fotos las podemos encontrar en formato http://pokeapi.co/media/img/'id'.png, por tanto
+				//var photoPokemon = $('<img src="http://pokeapi.co/media/img/' + idPokemon + '.png" id="img' + i.toString() + '"></img>');
+
+				//var large = tipoPokemon.length;
+				/*for (i=1 ; i<=large ; i++){
+					$(".mis-pokemon").append('<div class="profilePokemon" id="number' + i.toString() + '"></div>');
+					$(".profilePokemon").append("#img" + i.toString()).append('<p class="nombre">' + namePokemon + '</p>');
+				}*/
+
+				//var photoPokemon = $('<img src="http://pokeapi.co/media/img/' + idPokemon + '.png" id="number' + idPokemon + '"></img>');
+				$(".mis-pokemon").append('<div class="profilePokemon" id="profile' + idPokemon.toString() + '"></div>');
+				$("#profile" + idPokemon.toString()).append('<img src="http://pokeapi.co/media/img/' + idPokemon + '.png" class="img-responsive" id="number' + idPokemon + '"></img>').append('<p class="nombre center-align">' + namePokemon + '</p>');
+
+				//será los div contenedores de la imagen y nombre del pokemon donde se activara el modal
+				$("#profile" + idPokemon.toString()).attr("href","#modal" + idPokemon.toString());
+
+				//modal
+				$("#profile" + idPokemon.toString()).append('<div id="modal' + idPokemon.toString() + '" class="modal modal-fixed-footer"></div>').append('<div class="modal-content" id="modal-content-' + idPokemon.toString() + '"></div>').append('<div class="modal-footer" id="modal-footer-' + idPokemon.toString() + '"></div>');
+				//se divide en columnas el interior de los modales
+				$(".modal-content").append('<div class="col m5" id="img-modal-' + idPokemon.toString() + '"></div>').append('<div class="col m7" id="info-modal-' + idPokemon.toString() + '"></div>');
+				//se agrega imagen al modal
+				$("img-modal-" + idPokemon.toString()).append('<img src="http://pokeapi.co/media/img/' + idPokemon + '.png" class="img-responsive"></img>');
+				//se agrega info (nombre, descripcion, etc)
+				$("info-modal-" + idPokemon.toString()).append('<p class="nombre center-align">' + namePokemon + '</p>');
+				//se agrega el boton "cerrar" al footer del modal
+				$("#modal-footer-" + idPokemon.toString()).append('<a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat" id="btn-' + idPokemon.toString() + '">Cerrar</a>');
+			}
 		});
 		/*var name = "";
 		var url = "";
@@ -35,8 +82,8 @@ $(document).ready(function(){
 		});*/
 	}
 
-	var armarTemplate = function(gif,url){
+	/*var armarTemplate = function(gif,url){
 		var t = "<div class='elemento'><img src='" + gif + "'/><a href='" + url + "'>Ver más</a></div>";
 		return t;
-	}
+	}*/
 });
